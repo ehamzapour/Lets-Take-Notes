ba66d8d01b11b5f294d61f3aa3c119125e683396
Elham Hamzapour <ehamzapour@gmail.com>
2022-11-01 15:25:55 -0500

    added get, post, delete routes to api.js

---
+++ routes/api.js
@@ -0,0 +1,37 @@
+const api = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+const uuid = require('uuid');
+
+
+
+    //GET
+    api.get('/notes', (req, res) =>{
+        res.sendFile(path.join(__dirname, '../db/db.json'));
+    });
+
+    //POST
+    app.post('/api/notes', (req, res) => {
+        let db = fs.readFileSync('db/db.json');
+        db = JSON.parse(db);
+        res.json(db);
+
+        let userNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uuid.v4(),
+        };
+
+        db.push(userNote);
+        fs.writeFileSync('db/db.json', JSON.stringify(db));
+        res.json(db);
+    });
+
+    //DELETE
+    app.delete('/api/notes/:id', (req, res) => {
+        let db = JSON.parse(fs.readFileSync('db/db.json'))
+        let filter = db.filter(item => item.id !== req.params.id);
+        res.json(filter);
+    })
+
+    module.exports = api;
