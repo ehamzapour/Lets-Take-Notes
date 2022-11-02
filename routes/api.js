ea02677d89077a8c57832af2671dd4045426ccb4
Elham Hamzapour <ehamzapour@gmail.com>
2022-11-01 20:55:59 -0500

    got help and reconstructed coding

--- routes/api.js
+++ routes/api.js
@@ -3,36 +3,90 @@ const fs = require('fs');
 const path = require('path');
 const uuid = require('uuid');
 
+const util = require('util');
+const readFromFile = util.promisify(fs.readFile);
 
-module.exports = (app) => {
+
+
 
     //GET
     api.get('/notes', (req, res) =>{
-        res.sendFile(path.join(__dirname, '../db/db.json'));
+        readFromFile('./db/db.json')
+        .then((data) => {
+            res.json(JSON.parse(data))
+        })
     });
 
     //POST
-    api.post('/api/notes', (req, res) => {
-        let db = fs.readFileSync('db/db.json');
-        db = JSON.parse(db);
-        res.json(db);
+    api.post('/notes', (req, res) => {
+        console.log(req.body);
 
-        let userNote = {
-            title: req.body.title,
-            text: req.body.text,
-            id: uuid.v4(),
-        };
+        const {title, text } = req.body;
 
-        db.push(userNote);
-        fs.writeFileSync('db/db.json', JSON.stringify(db));
-        res.json(db);
+        if (req.body) {
+            const note = {
+                title,
+                text,
+                id: uuid.v4()
+            };
+        fs.readFile('/db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                const parseData = JSON.parse(data);
+                parseData.push(note);
+                fs.writeFile('./db/db.json', JSON.stringify(parseData), (err) => {
+                    if(err) {
+                        console.log(err)
+                    } else {
+                        console.log('Notes Created!')
+                    }
+                })
+            }
+        })
+        }
     });
 
     //DELETE
-    api.delete('/api/notes/:id', (req, res) => {
-        let db = JSON.parse(fs.readFileSync('db/db.json'))
-        let filter = db.filter(item => item.id !== req.params.id);
-        fs.writeFileSync('db/db.json', JSON.stringify(filter));
-        res.json(filter);
-    })
-};
+    api.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        fs.readFile('./db/db.json', (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                let db = JSON.parse(data);
+                const filter = db.filter(values => values.id != id);
+
+                fs.writeFile('./db/db.json', JSON.stringify(filter), 'utf8', (err) => {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log('Deleted!')
+                        res.end();
+                    }
+                })
+            }
+        })
+    });
+
+
+module.exports = api;
+
+    // let db = fs.readFileSync('db/db.json');
+        // db = JSON.parse(db);
+        // res.json(db);
+
+        // let userNote = {
+        //     title: req.body.title,
+        //     text: req.body.text,
+        //     id: uuid.v4(),
+        // };
+
+        // db.push(userNote);
+        // fs.writeFileSync('db/db.json', JSON.stringify(db));
+        // res.json(db);
+
+         // let db = JSON.parse(fs.readFileSync('db/db.json'))
+        // let filter = db.filter(item => item.id !== req.params.id);
+        // fs.writeFileSync('db/db.json', JSON.stringify(filter));
+        // res.json(filter);
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,16 +1,16 @@
 const html = require('express').Router();
 const path = require('path');
 
-module.exports = (app) => {
+
 
     //Get route for notes.html file
-    app.get('/notes', (req, res) => {
+    html.get('/notes', (req, res) => {
         res.sendFile(path.json(__dirname, '../public/notes.html'))
     });
 
     //Get * return index.html file
-    app.get('*', (req, res) => {
+    html.get('*', (req, res) => {
         res.sendFile(path.json(__dirname, '../public/index.html'))
     });
 
-};
+module.exports = html;
